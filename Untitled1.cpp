27535cd6da38fc3b19886984afd9600ccdce0f7c
akshayamoorthy05 <akshayamoorthyy@gmail.com>
2026-07-09 09:21:33 +0530

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,10 @@
+# include<stdio.h>
+int main(){
+	int a[5]={10,20,30,40,50};
+	printf("%d\n",a[0]);
+	printf("%d\n",a[1]);
+	printf("%d\n",a[2]);
+	printf("%d\n",a[3]);
+	printf("%d\n",a[4]);
+	return 0;
+}
